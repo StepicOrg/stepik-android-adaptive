fc425cd1816737c091bd7d858e86e9361f051f9e
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-07-19 18:45:14 +0300

    first level now is 1

--- app/src/main/java/org/stepik/android/adaptive/pdd/util/ExpUtil.java
+++ app/src/main/java/org/stepik/android/adaptive/pdd/util/ExpUtil.java
@@ -25,19 +25,19 @@ public class ExpUtil {
     }
 
     public static long getCurrentLevel(long exp) {
-        if (exp < 1) return 0;
-        if (exp < 2) return 1;
-        if (exp < 5) return 2;
+        if (exp < 1) return 1;
+        if (exp < 2) return 2;
+        if (exp < 5) return 3;
 
-        return 3 + (long) (Math.log(exp / 5) / Math.log(2));
+        return 4 + (long) (Math.log(exp / 5) / Math.log(2));
     }
 
     public static long getNextLevelExp(long currentLevel) {
-        if (currentLevel == 0) return 1;
-        if (currentLevel == 1) return 2;
-        if (currentLevel == 2) return 5;
+        if (currentLevel == 1) return 1;
+        if (currentLevel == 2) return 2;
+        if (currentLevel == 3) return 5;
 
-        return 5 * (long) Math.pow(2, currentLevel - 2);
+        return 5 * (long) Math.pow(2, currentLevel - 3);
     }
 
     public static void resetStreak() {
