9b7af739132608e304f2b3b39adbcb5c012770b3
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-08-02 11:48:29 +0300

    uniformity

--- app/src/main/java/org/stepik/android/adaptive/pdd/ui/view/morphing/MorphingHelper.kt
+++ app/src/main/java/org/stepik/android/adaptive/pdd/ui/view/morphing/MorphingHelper.kt
@@ -1,15 +1,16 @@
 package org.stepik.android.adaptive.pdd.ui.view.morphing
 
 import android.widget.TextView
+import org.stepik.android.adaptive.pdd.R
 
 object MorphingHelper {
 
     @JvmStatic
     fun morphStreakHeaderToIncBubble(header: MorphingView, inc: TextView) =
         MorphingAnimation(header, MorphingView.MorphParams(
-                cornerRadius = 32f,
+                cornerRadius = header.context.resources.getDimension(R.dimen.exp_bubble_corner_radius),
 
-                marginRight = 32,
+                marginRight = header.context.resources.getDimension(R.dimen.exp_bubble_margin).toInt(),
 
                 width = inc.width,
                 height = inc.height,
