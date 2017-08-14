fc0a7db5f083ca5d4c1f1d167f3154bdbd549210
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-08-14 16:14:21 +0300

    rating proxy for submissions

--- app/src/main/java/org/stepik/android/adaptive/pdd/api/SubmissionRequest.java
+++ app/src/main/java/org/stepik/android/adaptive/pdd/api/SubmissionRequest.java
@@ -1,9 +1,13 @@
 package org.stepik.android.adaptive.pdd.api;
 
+import org.stepik.android.adaptive.pdd.Config;
+import org.stepik.android.adaptive.pdd.data.SharedPreferenceMgr;
 import org.stepik.android.adaptive.pdd.data.model.Submission;
 
 public final class SubmissionRequest {
     private final Submission submission;
+    private final long course = Config.getInstance().getCourseId();
+    private final long user = SharedPreferenceMgr.getInstance().getProfileId();
 
     public SubmissionRequest(final Submission submission) {
         this.submission = submission;
