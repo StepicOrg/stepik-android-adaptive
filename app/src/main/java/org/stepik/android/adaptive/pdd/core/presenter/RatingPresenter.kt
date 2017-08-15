851dcefd59e65da6a2279ecaf93ffdacb0532405
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-08-15 20:37:10 +0300

    multiple rating periods & error handling

--- app/src/main/java/org/stepik/android/adaptive/pdd/core/presenter/RatingPresenter.kt
+++ app/src/main/java/org/stepik/android/adaptive/pdd/core/presenter/RatingPresenter.kt
@@ -1,7 +1,10 @@
 package org.stepik.android.adaptive.pdd.core.presenter
 
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import org.stepik.android.adaptive.pdd.api.API
 import org.stepik.android.adaptive.pdd.core.presenter.contracts.RatingView
 import org.stepik.android.adaptive.pdd.data.model.RatingItem
@@ -11,30 +14,73 @@ import org.stepik.android.adaptive.pdd.util.RatingNamesGenerator
 class RatingPresenter : PresenterBase<RatingView>() {
     companion object : PresenterFactory<RatingPresenter> {
         override fun create() = RatingPresenter()
+
+        @JvmStatic
+        private val ITEMS_PER_PAGE = 50
+
+        @JvmStatic
+        private val RATING_PERIODS = arrayOf(0, 7)
     }
 
-    private val adapter = RatingAdapter()
+    private val adapters = RATING_PERIODS.map { RatingAdapter() }
+
+    private val compositeDisposable = CompositeDisposable()
+    private val retrySubject = PublishSubject.create<Int>()
 
     private var isError = false
 
+    private var ratingPeriod = 0
+
+    private var periodsLoaded = 0
+
     init {
-        API.getInstance().getRating(50, 0)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ adapter.add(it.mapIndexed { index, (rank, _, exp, user) ->
-                    RatingItem(
-                            if (rank == 0) index + 1 else rank,
-                            RatingNamesGenerator.getName(user),
-                            exp,
-                            user
-                    )
-                }) }, { onError() })
+        RATING_PERIODS.forEachIndexed { pos, period ->
+            compositeDisposable.add(API.getInstance().getRating(ITEMS_PER_PAGE, period)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnError { onError() }
+                    .retryWhen { x -> x.zipWith(retrySubject, BiFunction<Throwable, Int, Throwable> { a, _ -> a }) }
+                    .subscribe({
+                        adapters[pos].add(prepareRatingItems(it))
+                        periodsLoaded++
+                        onLoadComplete()
+                    }, { onError() }))
+        }
+    }
+
+    private fun prepareRatingItems(data: List<RatingItem>) =
+            data.mapIndexed { index, (rank, _, exp, user) ->
+                RatingItem(
+                        if (rank == 0) index + 1 else rank,
+                        RatingNamesGenerator.getName(user),
+                        exp,
+                        user
+                )
+            }
+
+
+    fun changeRatingPeriod(pos: Int) {
+        this.ratingPeriod = pos
+        view?.onRatingAdapter(adapters[pos])
     }
 
     override fun attachView(view: RatingView) {
         super.attachView(view)
 
-        view.onRatingAdapter(adapter)
+        view.onRatingAdapter(adapters[ratingPeriod])
+        view.onLoading()
+
+        if (isError) {
+            view.onError()
+        } else {
+            onLoadComplete()
+        }
+    }
+
+    private fun onLoadComplete() {
+        if (periodsLoaded == RATING_PERIODS.size) {
+            view?.onComplete()
+        }
     }
 
     private fun onError() {
@@ -42,5 +88,12 @@ class RatingPresenter : PresenterBase<RatingView>() {
         view?.onError()
     }
 
-    override fun destroy() {}
+    fun retry() {
+        isError = false
+        retrySubject.onNext(0)
+    }
+
+    override fun destroy() {
+        compositeDisposable.dispose()
+    }
 }
--- app/src/main/java/org/stepik/android/adaptive/pdd/ui/fragment/RatingFragment.kt
+++ app/src/main/java/org/stepik/android/adaptive/pdd/ui/fragment/RatingFragment.kt
@@ -4,46 +4,67 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import org.stepik.android.adaptive.pdd.R
 import org.stepik.android.adaptive.pdd.core.presenter.BasePresenterFragment
 import org.stepik.android.adaptive.pdd.core.presenter.PresenterFactory
 import org.stepik.android.adaptive.pdd.core.presenter.RatingPresenter
 import org.stepik.android.adaptive.pdd.core.presenter.contracts.RatingView
-import org.stepik.android.adaptive.pdd.databinding.RecyclerViewBinding
+import org.stepik.android.adaptive.pdd.databinding.FragmentRatingBinding
 import org.stepik.android.adaptive.pdd.ui.adapter.RatingAdapter
 
 class RatingFragment : BasePresenterFragment<RatingPresenter, RatingView>(), RatingView {
-    private lateinit var recycler: RecyclerView
+    private lateinit var binding: FragmentRatingBinding
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        recycler = RecyclerViewBinding.inflate(inflater, container, false).recycler
-        recycler.layoutManager = LinearLayoutManager(context)
+        binding = FragmentRatingBinding.inflate(inflater, container, false)
+        binding.recycler.layoutManager = LinearLayoutManager(context)
 
         val divider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
         divider.setDrawable(ContextCompat.getDrawable(context, R.drawable.stroke))
-        recycler.addItemDecoration(divider)
+        binding.recycler.addItemDecoration(divider)
 
-        return recycler
+        val spinnerAdapter = ArrayAdapter<CharSequence>(context, R.layout.item_rating_period, context.resources.getStringArray(R.array.rating_periods))
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = spinnerAdapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                presenter?.changeRatingPeriod(pos)
+            }
+        }
+
+        binding.tryAgain.setOnClickListener { presenter?.retry() }
+        return binding.root
     }
 
     override fun onLoading() {
-
+        binding.error.visibility = View.GONE
+        binding.progress.visibility = View.VISIBLE
+        binding.container.visibility = View.GONE
     }
 
     override fun onError() {
+        binding.error.visibility = View.VISIBLE
+        binding.progress.visibility = View.GONE
+        binding.container.visibility = View.GONE
     }
 
     override fun onComplete() {
+        binding.error.visibility = View.GONE
+        binding.progress.visibility = View.GONE
+        binding.container.visibility = View.VISIBLE
     }
 
     override fun onRatingAdapter(adapter: RatingAdapter) {
-        recycler.adapter = adapter
+        binding.recycler.adapter = adapter
     }
-
 
     private var presenter : RatingPresenter? = null
 
