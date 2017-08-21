cf235ebeff69e2d0e0be28f3915f50637c010712
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-08-21 13:20:58 +0300

    achievements event logic

--- app/src/main/java/org/stepik/android/adaptive/pdd/data/model/Achievement.kt
+++ app/src/main/java/org/stepik/android/adaptive/pdd/data/model/Achievement.kt
@@ -1,6 +1,8 @@
 package org.stepik.android.adaptive.pdd.data.model
 
 import android.support.annotation.DrawableRes
+import org.stepik.android.adaptive.pdd.data.SharedPreferenceMgr
+import org.stepik.android.adaptive.pdd.util.AchievementManager
 
 
 data class Achievement(
@@ -8,6 +10,17 @@ data class Achievement(
         val description: String,
 
         val path: String,
-        val eventType: String,
-        @DrawableRes val icon: Int
-)
+        val eventType: AchievementManager.Event,
+
+        val targetValue: Long,
+
+        @DrawableRes val icon: Int,
+
+        val showProgress: Boolean = true
+) {
+    var currentValue: Long
+        get() = SharedPreferenceMgr.getInstance().getLong(path)
+        set(value) = SharedPreferenceMgr.getInstance().saveLong(path, value)
+
+    fun isComplete() = currentValue >= targetValue
+}
--- app/src/main/java/org/stepik/android/adaptive/pdd/util/AchievementManager.kt
+++ app/src/main/java/org/stepik/android/adaptive/pdd/util/AchievementManager.kt
@@ -1,24 +1,33 @@
 package org.stepik.android.adaptive.pdd.util
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.support.annotation.DrawableRes
 import android.support.annotation.IntegerRes
 import android.support.annotation.StringRes
-import io.reactivex.Completable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import org.stepik.android.adaptive.pdd.R
-import org.stepik.android.adaptive.pdd.Util
 import org.stepik.android.adaptive.pdd.core.presenter.Presenter
 import org.stepik.android.adaptive.pdd.core.presenter.contracts.AchievementView
+import org.stepik.android.adaptive.pdd.data.SharedPreferenceMgr
 import org.stepik.android.adaptive.pdd.data.model.Achievement
-import java.util.concurrent.TimeUnit
+import java.util.*
 
 object AchievementManager : Presenter<AchievementView> {
     private val views = HashSet<AchievementView>()
 
-    private val achievements = ArrayList<Achievement>()
+    val achievements = ArrayList<Achievement>()
+
+    private val queue = ArrayDeque<Achievement>()
 
     private lateinit var prefix : String
+
+    enum class Event {
+        ONBOARDING,
+        EXP,
+        STREAK,
+        DAYS,
+        LEVEL
+    }
 
     fun init(context: Context) {
         prefix = context.getString(R.string.ach_prefix)
@@ -27,6 +36,8 @@ object AchievementManager : Presenter<AchievementView> {
         initStreakAchievements(context)
         initDaysAchievements(context)
         initLevelAchievements(context)
+
+        sync()
     }
 
     private fun initOnboardingAchievement(context: Context) {
@@ -34,15 +45,17 @@ object AchievementManager : Presenter<AchievementView> {
                 context.getString(R.string.ach_onboarding_title),
                 context.getString(R.string.ach_onboarding_description),
                 prefix + context.getString(R.string.ach_onboarding_prefix),
-                context.getString(R.string.ach_onboarding_event),
-                -1
+                Event.ONBOARDING,
+                1,
+                -1,
+                false
         ))
     }
 
     private fun initExpAchievements(context: Context) {
         initAchievementGroup(context,
                 R.string.ach_exp_prefix,
-                R.string.ach_exp_event,
+                Event.EXP,
                 R.array.ach_exp_titles,
                 R.string.ach_exp_description,
                 R.array.ach_exp_values,
@@ -52,7 +65,7 @@ object AchievementManager : Presenter<AchievementView> {
     private fun initStreakAchievements(context: Context) {
         initAchievementGroup(context,
                 R.string.ach_streak_prefix,
-                R.string.ach_streak_event,
+                Event.STREAK,
                 R.array.ach_streak_titles,
                 R.string.ach_streak_description,
                 R.array.ach_streak_values,
@@ -62,7 +75,7 @@ object AchievementManager : Presenter<AchievementView> {
     private fun initDaysAchievements(context: Context) {
         initAchievementGroup(context,
                 R.string.ach_days_prefix,
-                R.string.ach_days_event,
+                Event.DAYS,
                 R.array.ach_days_titles,
                 R.string.ach_days_description,
                 R.array.ach_days_values,
@@ -72,7 +85,7 @@ object AchievementManager : Presenter<AchievementView> {
     private fun initLevelAchievements(context: Context) {
         initAchievementGroup(context,
                 R.string.ach_level_prefix,
-                R.string.ach_level_event,
+                Event.LEVEL,
                 R.array.ach_level_titles,
                 R.string.ach_level_description,
                 R.array.ach_level_values,
@@ -81,14 +94,13 @@ object AchievementManager : Presenter<AchievementView> {
 
     private fun initAchievementGroup(context: Context,
                                      @StringRes typePrefixRes: Int,
-                                     @StringRes eventTypeRes: Int,
+                                     event: Event,
                                      @StringRes titlesRes: Int,
                                      @StringRes descriptionRes: Int,
                                      @IntegerRes valuesRes: Int,
                                      @DrawableRes iconsRes: Int) {
         val titles = context.resources.getStringArray(titlesRes)
         val values = context.resources.getIntArray(valuesRes)
-        val event = context.getString(eventTypeRes)
 
 //        val drawables = context.resources.obtainTypedArray(iconsRes)
 
@@ -98,6 +110,8 @@ object AchievementManager : Presenter<AchievementView> {
                     context.getString(descriptionRes, values[index]),
                     prefix + context.getString(typePrefixRes, values[index]),
                     event,
+                    values[index].toLong(),
+
                     -1) //drawables.getResourceId(index, -1))
         })
 
@@ -106,22 +120,47 @@ object AchievementManager : Presenter<AchievementView> {
 
     override fun attachView(view: AchievementView) {
         views.add(view)
-
-        Completable.complete()
-                .delay(2, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { onAchievement(achievements[Util.getRandomNumberBetween(0, achievements.size - 1)]) }
-
+        notifyQueue()
     }
 
     override fun detachView(view: AchievementView) {
         views.remove(view)
     }
 
+    private fun notifyQueue() {
+        if (queue.isNotEmpty() && views.isNotEmpty())
+            onAchievement(queue.poll())
+    }
+
     private fun onAchievement(achievement: Achievement) {
         views.forEach { it.showAchievement(achievement) }
     }
 
+    fun onEvent(event: Event, value: Long, show: Boolean = true) {
+        achievements.filter { it.eventType == event && !it.isComplete() }.forEach {
+            it.currentValue = Math.min(it.targetValue, Math.max(value, it.currentValue))
+            if (it.isComplete() && show) {
+                queue.add(it) // todo
+                notifyQueue()
+            }
+        }
+    }
+
+
+    /**
+     * Method to sync achievements and stats
+     */
+    fun sync() {
+        val exp = ExpUtil.getExp()
+        onEvent(Event.EXP, exp, false)
+        onEvent(Event.STREAK, ExpUtil.getStreak(), false)
+        onEvent(Event.LEVEL, ExpUtil.getCurrentLevel(exp), false)
+        onEvent(Event.DAYS, DailyRewardManager.getCurrentRewardDay(), false)
+
+        if (SharedPreferenceMgr.getInstance().isNotFirstTime) {
+            onEvent(Event.ONBOARDING, 1, false)
+        }
+    }
 
     override fun destroy() {}
 }
