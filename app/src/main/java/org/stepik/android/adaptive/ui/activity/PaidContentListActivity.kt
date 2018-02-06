f51ee4917915792c22d986fb5ff62df68bc8e66d
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-02-06 21:32:48 +0300

    add recycler view to paid content screen

--- app/src/main/java/org/stepik/android/adaptive/ui/activity/PaidContentListActivity.kt
+++ app/src/main/java/org/stepik/android/adaptive/ui/activity/PaidContentListActivity.kt
@@ -1,6 +1,8 @@
 package org.stepik.android.adaptive.ui.activity
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.recycler_view.*
 import org.stepik.android.adaptive.R
@@ -9,12 +11,15 @@ import org.stepik.android.adaptive.core.presenter.PaidContentPresenter
 import org.stepik.android.adaptive.core.presenter.contracts.PaidContentView
 
 class PaidContentListActivity : BasePresenterActivity<PaidContentPresenter, PaidContentView>(), PaidContentView {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recycler_view)
 
         recycler.layoutManager = LinearLayoutManager(this)
+
+        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        divider.setDrawable(ContextCompat.getDrawable(this, R.drawable.stroke))
+        recycler.addItemDecoration(divider)
     }
 
     override fun getPresenterFactory() = PaidContentPresenter.Companion
