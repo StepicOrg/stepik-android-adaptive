5d0507f8dd9a6f8965dad36f182ab061a52ead59
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-02-14 19:21:31 +0300

    add billing to App class

--- app/src/main/java/org/stepik/android/adaptive/App.kt
+++ app/src/main/java/org/stepik/android/adaptive/App.kt
@@ -1,8 +1,15 @@
 package org.stepik.android.adaptive
 
 import android.support.multidex.MultiDexApplication
+import org.solovyev.android.checkout.Billing
 
 class App : MultiDexApplication() {
+    val billing by lazy {
+        Billing(this, object : Billing.DefaultConfiguration() {
+            override fun getPublicKey() = Config.getInstance().appPublicLicenseKey
+        })
+    }
+
     override fun onCreate() {
         super.onCreate()
         Util.initMgr(applicationContext)
