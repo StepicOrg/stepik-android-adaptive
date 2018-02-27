018fbf7cbe23df83c3eff8f41fa48771081640cf
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-02-27 19:55:52 +0300

    new questions cards style

--- app/src/toefl3150/java/org/stepik/android/adaptive/data/model/QuestionsPack.kt
+++ app/src/toefl3150/java/org/stepik/android/adaptive/data/model/QuestionsPack.kt
@@ -1,10 +1,52 @@
 package org.stepik.android.adaptive.data.model
 
-enum class QuestionsPack(val id: String, val courseId: Long, val isFree: Boolean = false) {
-    Basic("questions_pack_basic", 3150, isFree = true),
-    Medium("questions_pack_medium", 6243),
-    Pro("questions_pack_pro", 6312),
-    Full("questions_pack_full", 6315);
+import android.support.annotation.ColorInt
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+import org.stepik.android.adaptive.R
+
+enum class QuestionsPack(
+        val id: String,
+        val courseId: Long,
+        val size: Int,
+        @StringRes   val difficulty: Int,
+        @DrawableRes val background: Int,
+        @ColorInt    val textColor: Int = 0xFFFFFF,
+        val isFree: Boolean = false) {
+    Basic(
+            id          = "questions_pack_basic",
+            courseId    = 3150,
+            size        = 300,
+            difficulty  = R.string.questions_difficulty_mixed,
+            background  = R.drawable.pack_bg_basic,
+            textColor   = 0x495057,
+            isFree      = true
+    ),
+
+    Medium(
+            id          = "questions_pack_medium",
+            courseId    = 6243,
+            size        = 718,
+            difficulty  = R.string.questions_difficulty_medium,
+            background  = R.drawable.pack_bg_medium
+    ),
+
+    Pro(
+            id          = "questions_pack_pro",
+            courseId    = 6312,
+            size        = 958,
+            difficulty  = R.string.questions_difficulty_high,
+            background  = R.drawable.pack_bg_pro,
+            isFree      = true
+    ),
+
+    Full(
+            id          = "questions_pack_full",
+            courseId    = 6315,
+            size        = 2574,
+            difficulty  = R.string.questions_difficulty_mixed,
+            background  = R.drawable.pack_bg_full
+    );
 
     companion object {
         fun getById(id: String) = values().find { it.id == id }
