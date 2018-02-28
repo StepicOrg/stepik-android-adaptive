c7a9f22c6f7e322dd22e88f63963dfa985d26d07
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-02-28 17:12:30 +0300

    fix non toefl targets

--- app/src/toefl3149/java/org/stepik/android/adaptive/data/model/QuestionsPack.kt
+++ app/src/toefl3149/java/org/stepik/android/adaptive/data/model/QuestionsPack.kt
@@ -1,5 +1,29 @@
 package org.stepik.android.adaptive.data.model
 
-enum class QuestionsPack(id: String, courseId: Long) {
-    Basic("questions_pack_basic", 3149)
+import android.support.annotation.ColorInt
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+import org.stepik.android.adaptive.R
+
+enum class QuestionsPack(
+        val id: String,
+        val courseId: Long,
+        var size: Int = 0,
+        @StringRes   val difficulty: Int,
+        @DrawableRes val background: Int,
+        @ColorInt    val textColor: Int = 0xFFFFFF,
+        val isFree: Boolean = false
+) {
+    Basic(
+        id          = "questions_pack_basic",
+        courseId    = 3149,
+        difficulty  = R.string.questions_difficulty_mixed,
+        background  = R.drawable.pack_bg_basic,
+        textColor   = 0x495057,
+        isFree      = true
+    );
+
+    companion object {
+        fun getById(id: String) = values().find { it.id == id }
+    }
 }
