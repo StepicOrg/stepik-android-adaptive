8dfd74e3ecf0e8ce6a297d48460a3748985deb22
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-03-14 14:51:11 +0300

    dialog icon follows packs icon

--- app/src/main/java/org/stepik/android/adaptive/ui/dialog/QuestionsPacksDialog.kt
+++ app/src/main/java/org/stepik/android/adaptive/ui/dialog/QuestionsPacksDialog.kt
@@ -2,10 +2,12 @@ package org.stepik.android.adaptive.ui.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.support.annotation.DrawableRes
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
 import kotlinx.android.synthetic.main.dialog_questions_packs.view.*
 import org.stepik.android.adaptive.R
+import org.stepik.android.adaptive.configuration.RemoteConfig
 import org.stepik.android.adaptive.core.ScreenManager
 import org.stepik.android.adaptive.data.AnalyticMgr
 import org.stepik.android.adaptive.data.AnalyticMgr.EVENT_ON_QUESTIONS_DIALOG_ACTION_CLICKED
@@ -29,6 +31,13 @@ class QuestionsPacksDialog : DialogFragment() {
             ScreenManager.showQuestionsPacksScreen(activity)
             dismiss()
         }
+
+        @DrawableRes val packsIconRes = if (RemoteConfig.getFirebaseConfig().getBoolean(RemoteConfig.QUESTIONS_PACKS_ICON_EXPERIMENT)) {
+            R.drawable.ic_questions_pack_basic
+        } else {
+            R.drawable.ic_packs
+        }
+        root.packsIcon.setImageResource(packsIconRes)
 
         alertDialogBuilder.setView(root)
         return alertDialogBuilder.create()
