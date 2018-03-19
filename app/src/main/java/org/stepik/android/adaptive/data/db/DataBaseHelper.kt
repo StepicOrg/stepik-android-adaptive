fd0ab27c366233becb603650377d4e1f504b3733
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-03-19 15:44:21 +0300

    update database

--- app/src/main/java/org/stepik/android/adaptive/data/db/DataBaseHelper.kt
+++ app/src/main/java/org/stepik/android/adaptive/data/db/DataBaseHelper.kt
@@ -3,22 +3,47 @@ package org.stepik.android.adaptive.data.db
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import org.stepik.android.adaptive.data.db.structure.BookmarksDbStructure
 import org.stepik.android.adaptive.data.db.structure.ExpDbStructure
 
 class DataBaseHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
     companion object {
         private const val DB_NAME = "stepik_adaptive_pdd"
-        private const val DB_VERSION = 1
+        private const val DB_VERSION = 2
     }
 
 
-    override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE ${ExpDbStructure.TABLE_NAME} (" +
+    override fun onCreate(db: SQLiteDatabase) {
+        createExpDatabase(db)
+
+        upgradeFrom1To2(db)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        if (oldVersion < 2) {
+            upgradeFrom1To2(db)
+        }
+    }
+
+    private fun upgradeFrom1To2(db: SQLiteDatabase) {
+        createBookmarksDatabase(db)
+    }
+
+    private fun createBookmarksDatabase(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE ${BookmarksDbStructure.TABLE_NAME} (" +
+                "${BookmarksDbStructure.Columns.STEP_ID} INTEGER, " +
+                "${BookmarksDbStructure.Columns.COURSE_ID} INTEGER, " +
+                "${BookmarksDbStructure.Columns.WORD} TEXT, " +
+                "${BookmarksDbStructure.Columns.DEFINITION} TEXT, " +
+                "${BookmarksDbStructure.Columns.DATE_ADDED} DATETIME DEFAULT CURRENT_TIMESTAMP," +
+                "PRIMARY KEY (${BookmarksDbStructure.Columns.STEP_ID}));")
+    }
+
+    private fun createExpDatabase(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE ${ExpDbStructure.TABLE_NAME} (" +
                 "${ExpDbStructure.Columns.EXP} INTEGER," +
                 "${ExpDbStructure.Columns.SOLVED_AT} DATETIME DEFAULT CURRENT_TIMESTAMP," +
                 "${ExpDbStructure.Columns.SUBMISSION_ID} INTEGER," +
                 "PRIMARY KEY (${ExpDbStructure.Columns.SOLVED_AT}, ${ExpDbStructure.Columns.SUBMISSION_ID}));")
     }
-
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
 }
