84597351f9b6086b64f83a852071aa91bfbabb92
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-03-27 12:56:11 +0300

    convert IntroActivity to kotlin

---
+++ app/src/main/java/org/stepik/android/adaptive/ui/activity/IntroActivity.kt
@@ -0,0 +1,7 @@
+package org.stepik.android.adaptive.ui.activity
+
+import org.stepik.android.adaptive.ui.fragment.OnboardingFragment
+
+class IntroActivity : FragmentActivity() {
+    override fun createFragment() = OnboardingFragment()
+}
