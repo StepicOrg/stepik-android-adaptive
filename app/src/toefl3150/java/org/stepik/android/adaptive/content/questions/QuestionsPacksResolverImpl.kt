cca8c34ec26342b66feba5539c9f899187cbd28e
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-03-29 19:07:25 +0300

    consts for level values

--- app/src/toefl3150/java/org/stepik/android/adaptive/content/questions/QuestionsPacksResolverImpl.kt
+++ app/src/toefl3150/java/org/stepik/android/adaptive/content/questions/QuestionsPacksResolverImpl.kt
@@ -11,16 +11,21 @@ class QuestionsPacksResolverImpl
 constructor(
         private val expManager: ExpManager
 ): QuestionsPacksResolver {
+    companion object {
+        private const val ADJECTIVES_PACK_TARGET_LEVEL = 10
+        private const val VERBS_PACK_TARGET_LEVEL = 12
+    }
+
     override fun isAvailableForFree(pack: QuestionsPack) = when(pack) {
         QuestionsPack.Basic      -> true
-        QuestionsPack.Adjectives -> expManager.getCurrentLevel(expManager.exp) >= 10
-        QuestionsPack.Verbs      -> expManager.getCurrentLevel(expManager.exp) >= 12
+        QuestionsPack.Adjectives -> expManager.getCurrentLevel(expManager.exp) >= ADJECTIVES_PACK_TARGET_LEVEL
+        QuestionsPack.Verbs      -> expManager.getCurrentLevel(expManager.exp) >= VERBS_PACK_TARGET_LEVEL
         else                     -> false
     }
 
     override fun calcProgress(pack: QuestionsPack) = when(pack) {
-        QuestionsPack.Adjectives -> min(expManager.exp * 100 / expManager.getNextLevelExp(10 - 1L), 100).toInt()
-        QuestionsPack.Verbs      -> min(expManager.exp * 100 / expManager.getNextLevelExp(12 - 1L), 100).toInt()
+        QuestionsPack.Adjectives -> min(expManager.exp * 100 / expManager.getNextLevelExp(ADJECTIVES_PACK_TARGET_LEVEL - 1L), 100).toInt()
+        QuestionsPack.Verbs      -> min(expManager.exp * 100 / expManager.getNextLevelExp(VERBS_PACK_TARGET_LEVEL - 1L), 100).toInt()
         else -> 0
     }
 }
