a74d227bfe6a09d750497710af92db89b3a50bad
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-04 15:11:41 +0300

    fix naming

--- app/src/main/java/org/stepik/android/adaptive/ui/activity/DescriptionActivity.kt
+++ app/src/main/java/org/stepik/android/adaptive/ui/activity/DescriptionActivity.kt
@@ -2,14 +2,14 @@ package org.stepik.android.adaptive.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.header_levels_screen.*
+import kotlinx.android.synthetic.main.header_description.*
 import org.stepik.android.adaptive.R
 
-class LevelsScreenActivity: AppCompatActivity() {
+class DescriptionActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_levels_screen)
+        setContentView(R.layout.activity_description)
 
         close.setOnClickListener { finish() }
     }
