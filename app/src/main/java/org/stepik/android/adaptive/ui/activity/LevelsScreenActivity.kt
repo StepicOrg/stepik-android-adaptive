f0eaa099f7a58a6c7cddac17c203fbb4ac7d1b3d
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-04 14:49:59 +0300

    header for levels screen

---
+++ app/src/main/java/org/stepik/android/adaptive/ui/activity/LevelsScreenActivity.kt
@@ -0,0 +1,13 @@
+package org.stepik.android.adaptive.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.stepik.android.adaptive.R
+
+class LevelsScreenActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_levels_screen)
+    }
+}
