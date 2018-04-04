8b4aaa0d12f03cbc21ab4ead55b8f8a04cb4637a
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-04 15:02:35 +0300

    close button

--- app/src/main/java/org/stepik/android/adaptive/ui/activity/LevelsScreenActivity.kt
+++ app/src/main/java/org/stepik/android/adaptive/ui/activity/LevelsScreenActivity.kt
@@ -2,6 +2,7 @@ package org.stepik.android.adaptive.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.header_levels_screen.*
 import org.stepik.android.adaptive.R
 
 class LevelsScreenActivity: AppCompatActivity() {
@@ -9,5 +10,7 @@ class LevelsScreenActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_levels_screen)
+
+        close.setOnClickListener { finish() }
     }
 }
