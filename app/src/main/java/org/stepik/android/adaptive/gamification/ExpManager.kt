461bfd46306066c5bffea68a6f42cfc5ebadf6f1
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-06 13:36:11 +0300

    update level formula for expManager

--- app/src/main/java/org/stepik/android/adaptive/gamification/ExpManager.kt
+++ app/src/main/java/org/stepik/android/adaptive/gamification/ExpManager.kt
@@ -1,5 +1,6 @@
 package org.stepik.android.adaptive.gamification
 
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import org.stepik.android.adaptive.api.Api
 import org.stepik.android.adaptive.data.Analytics
 import org.stepik.android.adaptive.data.SharedPreferenceHelper
@@ -10,6 +11,7 @@ import io.reactivex.Scheduler
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.functions.Consumer
 import io.reactivex.internal.functions.Functions
+import org.stepik.android.adaptive.configuration.RemoteConfig
 import org.stepik.android.adaptive.di.AppSingleton
 import org.stepik.android.adaptive.di.qualifiers.BackgroundScheduler
 import org.stepik.android.adaptive.gamification.achievements.AchievementEventPoster
@@ -28,11 +30,14 @@ constructor(
         private val achievementEventPoster: AchievementEventPoster,
         private val sharedPreferenceHelper: SharedPreferenceHelper,
         private val dataBaseMgr: DataBaseMgr,
-        private val analytics: Analytics
+        private val analytics: Analytics,
+        private val firebaseRemoteConfig: FirebaseRemoteConfig
 ) {
     companion object {
         private const val EXP_KEY = "exp_key"
         private const val STREAK_KEY = "streak_key"
+
+        private const val LEVEL_POW = 2.5
 
         fun syncRating(dataBaseMgr: DataBaseMgr, api: Api): Completable = dataBaseMgr.getExp().flatMapCompletable { e -> api.putRating(e) }
     }
@@ -74,17 +79,23 @@ constructor(
     }
 
     fun getCurrentLevel(exp: Long): Long {
-        if (exp < 5) return 1
-
-        val level = 2 + (Math.log((exp / 5).toDouble()) / Math.log(2.0)).toLong()
+        val level = if (firebaseRemoteConfig.getBoolean(RemoteConfig.EXP_LEVEL_FORMULA_EXPERIMENT)) {
+            Math.pow(exp.toDouble(), 1.0 / LEVEL_POW).toLong()
+        } else {
+            if (exp < 5) return 1
+            2 + (Math.log((exp / 5).toDouble()) / Math.log(2.0)).toLong()
+        }
 
         achievementEventPoster.onEvent(AchievementManager.Event.LEVEL, level, true)
 
         return level
     }
 
-    fun getNextLevelExp(currentLevel: Long) =
-            if (currentLevel == 1L) 5 else 5 * Math.pow(2.0, (currentLevel - 1).toDouble()).toLong()
+    fun getNextLevelExp(currentLevel: Long) = if (firebaseRemoteConfig.getBoolean(RemoteConfig.EXP_LEVEL_FORMULA_EXPERIMENT)) {
+        Math.ceil(Math.pow(currentLevel.toDouble() + 1, LEVEL_POW)).toLong()
+    } else {
+        if (currentLevel == 1L) 5 else 5 * Math.pow(2.0, (currentLevel - 1).toDouble()).toLong()
+    }
 
     fun resetStreak() {
         analytics.onStreakLost(streak)
