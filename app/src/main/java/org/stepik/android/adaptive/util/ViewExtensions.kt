af6bdec9456f56ac9057c1c19d36d9dd867dc537
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-11 17:25:00 +0300

    hide all children extension

--- app/src/main/java/org/stepik/android/adaptive/util/ViewExtensions.kt
+++ app/src/main/java/org/stepik/android/adaptive/util/ViewExtensions.kt
@@ -1,11 +1,18 @@
 package org.stepik.android.adaptive.util
 
 import android.view.View
+import android.view.ViewGroup
 
 fun View.changeVisibillity(needShow: Boolean) {
     visibility = if (needShow) {
         View.VISIBLE
     } else {
         View.GONE
+    }
+}
+
+fun ViewGroup.hideAllChildren() {
+    for (i in 0 until childCount) {
+        getChildAt(i).changeVisibillity(false)
     }
 }
