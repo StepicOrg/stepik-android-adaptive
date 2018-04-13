720648d361bec266030aba025e84ba53cee155ef
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-13 17:25:17 +0300

    states for edit profile view

--- app/src/main/java/org/stepik/android/adaptive/core/presenter/EditProfilePresenter.kt
+++ app/src/main/java/org/stepik/android/adaptive/core/presenter/EditProfilePresenter.kt
@@ -1,10 +1,58 @@
 package org.stepik.android.adaptive.core.presenter
 
+import io.reactivex.Scheduler
+import io.reactivex.disposables.CompositeDisposable
+import org.stepik.android.adaptive.api.profile.ProfileRepository
 import org.stepik.android.adaptive.core.presenter.contracts.EditProfileView
+import org.stepik.android.adaptive.data.model.Profile
+import org.stepik.android.adaptive.data.preference.ProfilePreferences
+import org.stepik.android.adaptive.di.qualifiers.BackgroundScheduler
+import org.stepik.android.adaptive.di.qualifiers.MainScheduler
+import org.stepik.android.adaptive.util.addDisposable
 import javax.inject.Inject
 
 class EditProfilePresenter
 @Inject
-constructor(): PresenterBase<EditProfileView>() {
-    override fun destroy() {}
+constructor(
+        private val profileRepository: ProfileRepository,
+
+        @BackgroundScheduler
+        private val backgroundScheduler: Scheduler,
+        @MainScheduler
+        private val mainScheduler: Scheduler,
+
+        private val profilePreferences: ProfilePreferences
+): PresenterBase<EditProfileView>() {
+    private val compositeDisposable = CompositeDisposable()
+
+    private var state: EditProfileView.State = EditProfileView.State.ProfileLoading
+        set(value) {
+            field = value
+            view?.setState(value)
+        }
+
+    init {
+        compositeDisposable addDisposable profileRepository.fetchProfile() // if profile is null we display an error
+                .subscribeOn(backgroundScheduler)
+                .observeOn(mainScheduler)
+                .subscribe({
+                    state = EditProfileView.State.ProfileLoaded(it)
+                }, {
+                    it.printStackTrace()
+                    state = EditProfileView.State.ProfileLoadingError
+                })
+    }
+
+    fun updateProfile(firstName: String, lastName: String) {
+        compositeDisposable addDisposable profileRepository.updateProfile(Profile(firstName = firstName, lastName = lastName))
+                .subscribe()
+    }
+
+    override fun attachView(view: EditProfileView) {
+        super.attachView(view)
+        view.setState(state)
+    }
+
+    override fun destroy() =
+            compositeDisposable.dispose()
 }
--- app/src/main/java/org/stepik/android/adaptive/core/presenter/contracts/EditProfileView.kt
+++ app/src/main/java/org/stepik/android/adaptive/core/presenter/contracts/EditProfileView.kt
@@ -1,4 +1,13 @@
 package org.stepik.android.adaptive.core.presenter.contracts
 
+import org.stepik.android.adaptive.data.model.Profile
+
 interface EditProfileView {
+    fun setState(state: State)
+
+    sealed class State {
+        object ProfileLoading: State()
+        class ProfileLoaded(val profile: Profile): State()
+        object ProfileLoadingError: State()
+    }
 }
