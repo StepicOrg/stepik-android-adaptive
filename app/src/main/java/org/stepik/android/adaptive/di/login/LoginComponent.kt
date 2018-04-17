fd5f413fb63f0b67eeb1c6d715fa6b880c0235b1
Ruslan Davletshin <smartmorefeed@gmail.com>
2018-04-17 14:54:04 +0300

    remove unused resources

--- app/src/main/java/org/stepik/android/adaptive/di/login/LoginComponent.kt
+++ app/src/main/java/org/stepik/android/adaptive/di/login/LoginComponent.kt
@@ -1,7 +1,6 @@
 package org.stepik.android.adaptive.di.login
 
 import dagger.Subcomponent
-import org.stepik.android.adaptive.ui.activity.EditProfileActivity
 import org.stepik.android.adaptive.ui.activity.LoginActivity
 import org.stepik.android.adaptive.ui.activity.RegisterActivity
 import org.stepik.android.adaptive.ui.activity.SocialAuthActivity
@@ -16,7 +15,6 @@ interface LoginComponent {
 
     fun inject(activity: SocialAuthActivity)
     fun inject(activity: LoginActivity)
-    fun inject(activity: EditProfileActivity)
     fun inject(activity: RegisterActivity)
 
     fun inject(dialog: RemindPasswordDialog)
