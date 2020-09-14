e6ace44123ea6385d40977a4922661ecb60f0e0f
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2020-09-15 00:30:57 +0300

    Fix annotations in remaining flavors

--- app/src/gmat3067/java/org/stepik/android/adaptive/content/questions/QuestionsPack.kt
+++ app/src/gmat3067/java/org/stepik/android/adaptive/content/questions/QuestionsPack.kt
@@ -1,8 +1,8 @@
 package org.stepik.android.adaptive.content.questions
 
-import android.support.annotation.ColorInt
-import android.support.annotation.DrawableRes
-import android.support.annotation.StringRes
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import org.stepik.android.adaptive.R
 
 enum class QuestionsPack(
